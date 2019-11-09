6d40659326369453cd5d2b2108295f3028536ff2
Jonathan Cui <jonathancui123@gmail.com>
2019-11-09 12:11:40 -0500

    nested promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,34 +1,41 @@
- console.log('Before');
- getUser(1, getRepositories);
- console.log('After');
- 
-function getRepositories(user){
-    getRepositories(user.gitHubUsername)
+function getUser(id){
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+            console.log('Db doing work');
+            resolve({id: id, gitName: 'Jonathan'});
+        }, 2000)
+    })
 }
 
-function getUser(id, getRepo){
-    setTimeout(()=>{
-        console.log('Db doing work');
-        var gitInfo = 
-        getRepo({id: id, gitName: 'Jonathan'}, getCommits);
-    }, 2000)
+function getRepositories(userinfo){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log(userinfo.gitName);
+            // console.log(`Reading ${userinfo.gitName}'s repos...`);
+            reject(new Error('big fat error'));
+            // resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000)
+    })
+    }
+
+function getCommits(repo){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`Looking for commits in: ${repo}`);
+            resolve(['commit1', 'commit2', 'commit3']);
+        }, 2000);
+    })
 }
-
-// function getRepositories(gitInfo, getCommits){
-//     setTimeout(()=>{
-//         console.log(gitInfo.gitName);
-//         // console.log(`Reading ${gitinfo.gitName}'s repos...`);
-//         getCommits(['repo1', 'repo2', 'repo3'], displayCommits());
-//     }, 2000)
-// }
-
-// function getCommits(repo, displayCommits){
-//     setTimeout(() => {
-//         console.log(`Looking for commits in: ${repo}`);
-//         displayCommits(['commit1', 'commit2', 'commit3']);
-//     }, 2000);
-// }
 
 function displayCommits(commits){
     console.log('Displaying commits: ', commits);
 }
+
+//In this file, the functions are named to avoid callback hell
+//Below, promises are used to avoid callback looping
+
+getUser(1)
+    .then(userInfo => getRepositories(userInfo))
+    .then(repos => getCommits(repos))
+    .then(commits => displayCommits(commits))
+    .catch(err => console.log('This should be printed upon error'));
