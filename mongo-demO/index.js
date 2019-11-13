6cb60354ac2ef3be83b0f657d91d3d28c3ef7e92
Jonathan Cui <jonathancui123@gmail.com>
2019-11-13 15:44:40 -0500

    Schema

--- mongo-demO/index.js
+++ mongo-demO/index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to the MongoDB...'))
     .catch(err => console.error('Could not connect to mongoDB', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String, 
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+})
