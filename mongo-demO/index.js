c3242a564fe43b219b60e7cae7cd6273c5ca4850
Jonathan Cui <jonathancui123@gmail.com>
2019-11-14 09:44:23 -0500

    Working on mongo

--- mongo-demO/index.js
+++ mongo-demO/index.js
@@ -11,3 +11,19 @@ const courseSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now },
     isPublished: Boolean
 })
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Ligma',
+        author: 'Jonathan',
+        tags: ['big', 'lig'],
+        isPublished: true
+    })
+
+    const ligma = await course.save();
+    console.log(ligma);
+}
+
+createCourse();
