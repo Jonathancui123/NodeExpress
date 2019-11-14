82876f53cb086057b25c458d7cdadb74ccefd87b
Jonathan Cui <jonathancui123@gmail.com>
2019-11-14 13:07:52 -0500

    Finished MongoDB query exercises

--- 07.1_mongoExercise/index.js
+++ 07.1_mongoExercise/index.js
@@ -1,3 +1,42 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/')
-    .then(() => {console.log('Connected to DB')})
+mongoose.connect('mongodb://localhost/mongo-exercises')
+    .then(() => { console.log('Connected to DB') })
+    .catch((err) => { console.err("Error caught: ", err) });
+
+const courseSchema = new mongoose.Schema({
+    _id: String,
+    tags: [String],
+    date: Date,
+    name: String,
+    author: String,
+    isPublished: Boolean,
+    price: Number
+})
+
+const courses = mongoose.model('courses', courseSchema);
+
+async function getCourses() {
+    var queriedCourses = await courses.find({ isPublished: true })
+        .or([{ price: { $gte: 15 } }, { name: /.*by.*/ }])
+        // .or([{ tags: 'frontend' }, { tags: 'backend' }])
+        .sort({ price: -1 })
+        .select({ name: 1, author: 1, price: 1 })
+    console.log(queriedCourses)
+}
+
+// getCourses();
+
+async function updateCourse(id) {
+    const course = await courses.findById(id);
+    if (!course) { console.log("course not found"); return; }
+
+    course.isPublished = true;
+    course.author = 'mE';
+
+    const result = await course.save();
+    console.log(result);
+}
+
+updateCourse('5a68ff090c553064a218a547')
+
+
