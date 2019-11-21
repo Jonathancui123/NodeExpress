1fc90ab2a521b3ef6492f2c563ed8ae0e92da125
Jonathan Cui <jonathancui123@gmail.com>
2019-11-21 12:50:38 -0500

    asdfghjkl

---
+++ 07.1_mongoExercise/Untitled-1.py
@@ -0,0 +1,11 @@
+run = False
+
+while not run:
+    name = input("Hello! What is your name? ").strip().lower()
+    if name in ['varun', 'carib', 'mokashi','v','vm', 'jonathan','jc','jon','jonathan cui','cui','j']:
+        print("omg ily {0} <3 :*".format(name))
+        break
+    else:
+        print("why rnt u varun or jc >:( print varun or jc next time")
+
+    
